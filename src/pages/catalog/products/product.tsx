ae9470de0a772da36203ebb27ed4cbedbf3bae9c
João Gomes <joaopfg.2002@gmail.com>
2020-10-29 11:50:26 +0000

    create next app

---
+++ src/pages/catalog/products/product.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+const Product: React.FC = () => {
+  return(
+    <div>
+        <h1>Product</h1>
+    </div>
+  );
+}
+
+export default Product;
